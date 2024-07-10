cc8570c352c1ffd3ebffd241a3d7b81f7c575c24
totgokhung123 <chutienbinh2003@gmail.com>
2024-07-10 17:09:57 +0700

    dang sua css

--- src/main/java/DoAnLTUngDung/DoAnLTUngDung/controller/HoaDonController.java
+++ src/main/java/DoAnLTUngDung/DoAnLTUngDung/controller/HoaDonController.java
@@ -1,4 +1,17 @@
 package DoAnLTUngDung.DoAnLTUngDung.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
 public class HoaDonController {
+    @GetMapping("/inhoadon")
+    public String checkout() {
+        return "HOADON/invoice-4";
+    }
 }
